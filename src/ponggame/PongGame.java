9280c7f7bb224acedb71cc50a31f38bad34429fe
MassiJimmy <92297534+MassiJimmy@users.noreply.github.com>
2023-12-03 12:59:57 +0100

    Initial commit

---
+++ src/ponggame/PongGame.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ponggame;
+
+public class PongGame {
+
+
+
+	public static void main(String[] args) {
+
+		
+
+		GameFrame frame = new GameFrame();
+
+		
+
+	}
+
+}
